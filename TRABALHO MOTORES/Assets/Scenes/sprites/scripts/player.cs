70ffcee0510deb3517f5489f43c5402bda2c809f
emii07 <wemilymartins31@gmail.com>
2025-02-06 11:57:45 -0300

    fase 2
    fase 2 concluida

--- TRABALHO MOTORES/Assets/Scenes/sprites/scripts/player.cs
+++ TRABALHO MOTORES/Assets/Scenes/sprites/scripts/player.cs
@@ -27,39 +27,42 @@ public class player : MonoBehaviour
             noChao = true;
         }
     }
-   void OnCollisionExit2D(Collision2D collision)
+    void OnCollisionExit2D(Collision2D collision)
        {
            if (collision.gameObject.tag == "chao")
            {
                noChao = false;
            }
-       }
-    
+       } 
     void Update()
-    {
-        if(Input.GetKey(KeyCode.LeftArrow))
-        {
-            gameObject.transform.position += new Vector3(-velocidade*Time.deltaTime,0,0);
-            //rigidbody2D.AddForce(new Vector2(-velocidade,0));
-            spriteRenderer.flipX = true;
-            Debug.Log("LeftArrow");
-            
-        } 
-        if(Input.GetKey(KeyCode.RightArrow))
-        {
-            gameObject.transform.position += new Vector3(velocidade*Time.deltaTime,0,0);
-            spriteRenderer.flipX = false;
-            Debug.Log("RightArrow");
+       { 
+           if(Input.GetKey(KeyCode.LeftArrow))
+           {
+               gameObject.transform.position += new Vector3(-velocidade*Time.deltaTime,0,0);
+               //rigidbody2D.AddForce(new Vector2(-velocidade,0));
+               spriteRenderer.flipX = true;
+               Debug.Log("LeftArrow");
+           }
            
-        }
-        
-        if (Input.GetKeyDown(KeyCode.Space) && noChao == true)
-        {
-            _rigidbody2D.AddForce(new Vector2(0, 1) * focaPulo,ForceMode2D.Impulse);
+           
+           if(Input.GetKey(KeyCode.RightArrow))
+           {
+               gameObject.transform.position += new Vector3(velocidade*Time.deltaTime,0,0);
+               //rigidbody2D.AddForce(new Vector2(velocidade,0));
+               spriteRenderer.flipX = false;
+               Debug.Log("RightArrow");
+           }
 
-            Debug.Log("Jump");
-        }
-    }
-}     
-    
+           if (Input.GetKeyDown(KeyCode.Space) && noChao == true)
+           {
+               _rigidbody2D.AddForce(new Vector2(0, 1) * focaPulo,ForceMode2D.Impulse);
+
+               Debug.Log("Jump");
+           }
+
+     
+
+
+       }
+}
 
